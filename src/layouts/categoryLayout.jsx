60d827628b4bced3889a115f08ee4bedfed749d1
le minh tri <leminhtri886@gmail.com>
2023-06-11 04:20:08 +0700

    Updaterps

--- src/layouts/categoryLayout.jsx
+++ src/layouts/categoryLayout.jsx
@@ -24,6 +24,7 @@ export default function CategoryLayout() {
   const { handleSort, page } = UsePaginate();
 
   useEffect(() => {
+    document.querySelector(".header .nav").classList.remove("active");
     scroll();
     if (url) {
       switch (url) {
